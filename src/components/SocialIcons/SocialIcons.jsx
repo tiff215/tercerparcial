6778cdd45547bbe8e4b52fefe0fe233e3ac018bc
Jakeb <tiffanyytg2006@gmail.com>
2026-04-27 16:12:12 -0600

    Initial commit

---
+++ src/components/SocialIcons/SocialIcons.jsx
@@ -0,0 +1,17 @@
+// Importamos estilos
+import "./SocialIcons.css";
+
+// Íconos simples para redes sociales
+function SocialIcons() {
+  return (
+    <div className="social-icons">
+
+      <a href="https://facebook.com" target="_blank">Facebook</a>
+      <a href="https://instagram.com" target="_blank">Instagram</a>
+      <a href="https://tiktok.com" target="_blank">TikTok</a>
+
+    </div>
+  );
+}
+
+export default SocialIcons;
